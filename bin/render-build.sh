bc9f665ea91f5415601030ea00dde4101302ad90
adhitstrife63 <adhitstrife63@gmail.com>
2024-11-22 14:04:56 +0800

    fix again

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+apt-get update && apt-get install -y \
+    build-essential \
+    libpq-dev \
+    libxml2-dev \
+    libxslt-dev \
+    zlib1g-dev
+
+# Install Bundler and dependencies
+gem install bundler
+bundle install
+
 set -o errexit
 
 bundle install
